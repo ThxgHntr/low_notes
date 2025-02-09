8cf29ce717fe342617b97f7e28dce78ee13a0903
trinhdamhuy <trinhdamhuy@gmail.com>
2025-02-09 11:47:24 +0700

    Update Android SDK versions and initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,13 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
   runApp(MyApp(settingsController: settingsController));
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
